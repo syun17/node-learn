430cf15ed36913fba938057636864fbe8ab0814f
syun17 <shunki.s777@gmail.com>
2025-07-06 22:45:00 +0900

    helloの削除

--- ex-gen-app/routes/hello.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('hello', { title: 'Hello', content: 'これはサブタイトルです' });
-});
-
-module.exports = router;
